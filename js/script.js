3453f97b4074bbd21980fdd4e65e1c71ffbfa742
deadfrominside <32361765+deadfrominside@users.noreply.github.com>
2017-10-29 11:07:49 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
-//SMOOTH PAGE SCROLL
-$(function() {
+$(window).load(function() {
+  $('#status').fadeOut();
+  $('#preloader').delay(350).fadeOut('slow');
+  $('body').delay(350).css({'overflow':'visible'});
+});
+
+$(document).ready(function(){ 
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -13,56 +19,26 @@ $(function() {
       }
     }
   });
-});
 
+  $("html").niceScroll({
+    cursorcolor:"#f74d65",
+    scrollspeed :"100",
+    cursorborder:"1px solid #f74d65",
+    horizrailenabled: "false",
+    cursorborderradius: "0px"
+  });
 
-//OWL CAROSEL TESTIMONIAL
-$('.owl-carousel').owlCarousel({
+  $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
-    dots:true,
-    dotsEach:true,
+    nav:true,
     responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
+      0:{items:1},
+      600:{items:2},
+      1000:{items:3}
     }
-})
+  });
 
-$(document).ready(
+  new WOW().init();
 
-  function() { 
-
-    $("html").niceScroll({
-    	cursorcolor:"#f74d65",
-    	scrollspeed :"100",
-    	cursorborder:"1px solid #f74d65",
-    	horizrailenabled: "false",
-    	cursorborderradius: "0px"
-    });
-
-  }
-
-);
-
-new WOW().init();
-
-
-    
-/*Preloader*/
-//<![CDATA[
-$(window).load(function() { // makes sure the whole site is loaded
-  $('#status').fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(350).css({'overflow':'visible'});
-})
-//]]>
-
-
+});
